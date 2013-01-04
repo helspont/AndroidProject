28cfc71a9bda39d5798df49cba60a3bc4db2f110
Tomáš Jiřík <tjirik@gmail.com>
2013-01-04 18:13:13 +0100

    raw funkcionality

--- EasyStore/src/com/easystore/app/MainActivity.java
+++ EasyStore/src/com/easystore/app/MainActivity.java
@@ -1,33 +1,35 @@
 package com.easystore.app;
 
-import java.io.UnsupportedEncodingException;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Calendar;
-
-
-
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends ListActivity/*Activity*/ {
 	
 	private GetDataFromDatabase mydata = null;
 	private ArrayList<StoreValueListQuest> data = null;
-
+	TextView utrataTextView;
 	protected Button add;
 	protected String[] item,value,temp;
+	protected int [] values;
 	protected TextView date;
 	protected TextView sum;
 	protected String table;
@@ -36,6 +38,8 @@ public class MainActivity extends ListActivity {
 	private int year;
 	private int month;
 	private int day;
+	private ListView list,list1;
+	private ArrayAdapter<String> listAdapter,listAdapter2 ;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -46,19 +50,57 @@ public class MainActivity extends ListActivity {
 		mydata = new GetDataFromDatabase(this, table);
 		// mydata = null;
 		data = mydata.getValueAll(table);
+		data = mydata.getValueAll(table);
+		utrataTextView = (TextView)findViewById(R.id.sum);
+		//list= (ListView)findViewById(R.id.list);
+		//list1= (ListView)findViewById(R.id.list1);
 		
 		item = new String[data.size()];
 		value = new String[data.size()];
 		temp = new String[data.size()];
+		values = new int[data.size()];
 		for (int i = 0; i < data.size(); i++) {
 
 			item[i] = data.get(i).getName();
 			value[i] = data.get(i).getValue();
-			temp[i] = item[i]+"     "+value[i];
+			values[i] = Integer.parseInt(value[i]);
+			
 		}
 		
+		long utrata = 0;
 		
-	setListAdapter(new ArrayAdapter<String>(this, R.layout.row,R.id.label,temp));
+		for (int i = 0; i < data.size(); i++) {
+			
+			utrata += values[i];
+			
+		}
+
+		utrataTextView.setText(""+utrata+" Kè");
+		
+		MySimpleArrayAdapter adapter = new MySimpleArrayAdapter(this, data);
+	    setListAdapter(adapter);
+		
+		//ArrayList<String> itemtList = new ArrayList<String>();
+		//ArrayList<String> valutList = new ArrayList<String>();
+	    //itemtList.addAll( Arrays.asList(item) );
+	    //valutList.addAll(Arrays.asList(value));
+	      
+	    // Create ArrayAdapter using the planet list.  
+	    //listAdapter = new ArrayAdapter<String>(this, R.layout.row,R.id.label, itemtList);
+	   // listAdapter2 = new ArrayAdapter<String>(this, R.layout.row,R.id.label, valutList);
+		
+		//CustomAdapter adapter = new CustomAdapter();
+		// setListAdapter(customAdapter);
+		//list2 = (ListView) findViewById(android.R.id.list);
+		//ArrayAdapter<String> adapter= new ArrayAdapter<String>(this, R.id.label,R.id.label1);
+	
+		//setListAdapter(new ArrayAdapter<String>(this, R.layout.row,R.id.label,item));
+		//setListAdapter(new ArrayAdapter<String>(this, R.layout.row,R.id.label1,value));
+	
+		//setListAdapter(new ArrayAdapter<String>(this,R.layout.simple?list,R.id.label,temp));
+	
+		//list.setAdapter(listAdapter);
+		//list1.setAdapter(listAdapter2);
 
 		add.setOnClickListener(new OnClickListener() {
 
@@ -86,7 +128,7 @@ public class MainActivity extends ListActivity {
 		// set current date into textview
 		date.setText(new StringBuilder()
 			// Month is 0 based, just add 1
-		.append(day).append("-").append(month + 1).append("-")
+		.append("Dnes je: ").append(day).append("-").append(month + 1).append("-")
 			.append(year).append(" "));
  
 	}
@@ -95,4 +137,82 @@ public class MainActivity extends ListActivity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	/**
+	class CustomAdapter extends BaseAdapter{
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// TODO Auto-generated method stub
+			mydata = new GetDataFromDatabase(getApplicationContext(),table);
+			// mydata = null;
+			table = "items";
+			data = mydata.getValueAll(table);
+			for (int i = 0; i < data.size(); i++) {
+
+				item[i] = data.get(i).getName();
+				value[i] = data.get(i).getValue();
+				//temp[i] = item[i]+"     "+value[i];
+			}
+			TextView textView = (TextView)findViewById(R.id.label);
+			textView.setText(item[position]);
+			
+			textView =(TextView) findViewById(R.id.label1);
+			textView.setText(value[position]);
+			//TextView textView1 = (TextView)findViewById(R.id.label1);
+			//textView1.setText(data.get(position).getValue());
+			return null;
+		}
+		
+	}*/
+	
+	private class MySimpleArrayAdapter extends ArrayAdapter<StoreValueListQuest> {
+		  private final Context context;
+		  private final ArrayList<StoreValueListQuest> values;
+
+		  public MySimpleArrayAdapter(Context context, ArrayList<StoreValueListQuest> values) {
+		    super(context, R.layout.row, values);
+		    this.context = context;
+		    this.values = values;
+		  }
+
+		  @Override
+		  public View getView(int position, View convertView, ViewGroup parent) {
+		    LayoutInflater inflater = (LayoutInflater) context
+		        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		    View rowView = inflater.inflate(R.layout.row, parent, false);
+		    TextView textView1 = (TextView) rowView.findViewById(R.id.itemTextView);
+		    TextView textView2 = (TextView) rowView.findViewById(R.id.costTextView);
+		    StoreValueListQuest message = values.get(position);
+
+          if (message != null)
+          {
+              textView1.setText(message.getName());
+              textView2.setText(message.getValue());
+          }
+
+		    return rowView;
+		  }
+		} 
+
+	
 }
+
